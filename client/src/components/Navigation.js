02166f9897c5606753506f2a40496443eea8a3b1
Chukwudi64 <chukwudiodoh64@gmail.com>
2023-10-21 19:18:11 +0100

    models

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Button } from 'react-bootstrap';
+import './Navigation.css';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Navigation = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>Ecommerce Application</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ms-auto">
+            <Nav.Link href="/login">Login</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import http from 'http';
 const server = http.createServer(app);
 import { Server } from 'socket.io';
@@ -8,6 +10,7 @@ const io = new Server(server, {
     cors: '*',
     methods: '*'
 });
+dotenv.config();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -15,4 +18,12 @@ app.use(express.json());
 
 server.listen(8080, () => {
     console.log('server running at port', 8080);
+})
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Connected to mongoDB'))
+.catch(err => console.log(err))
+
+mongoose.connection.on('error', err => {
+    console.log(err) 
 })
