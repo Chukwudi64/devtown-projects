81419560ed64eaecd56718950aba09aa0cbf5ad1
Chukwudi64 <chukwudiodoh64@gmail.com>
2023-10-21 15:41:50 +0100

    Home page

---
+++ client/src/Categories.js
@@ -0,0 +1,16 @@
+const categories = [
+    {
+        name: "Technology",
+        img: "https://images.unsplash.com/photo-1518997554305-5eea2f04e384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+    },
+    {
+        name: "Phones",
+        img: "https://images.unsplash.com/photo-1464380573004-8ca85a08751a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGhvbmV8ZW58MHx8MHx3aGl0ZXw%3D&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+        name: "Laptops",
+        img: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fHww"
+    }
+];
+
+export default categories
