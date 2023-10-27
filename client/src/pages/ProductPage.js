3f2026d9f28b239d3ef39e21d701528f7637111d
Chukwudi64 <chukwudiodoh64@gmail.com>
2023-10-27 21:25:28 +0100

    Similar products

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -4,6 +4,8 @@ import AliceCarousel from 'react-alice-carousel';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import Loading from '../components/Loading';
+import SimilarProduct from '../components/SimilarProduct';
 
 const ProductPage = () => {
     const {id} = useParams();
@@ -20,6 +22,19 @@ const ProductPage = () => {
         })
     }, [id])
     const images = product.pictures.map((picture) => <img className='product__carouel--image' src={picture.url} onDragStart={handleDragStart} /> )
+
+    if (!product) {
+        return <Loading />
+    }
+
+    let similarProducts = [];
+    if (similar) {
+        similarProducts = similar.map((product, idx) => (
+            <div className='item' data-value={idx}>
+                <SimilarProduct />
+            </div>
+        ))
+    }
 
   return (
     <Container className='pt-4' style={{position: 'relative'}}>
