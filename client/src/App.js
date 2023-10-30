1415c4e8b20e81b12a7568e03bb99069c7aec204
Chukwudi64 <chukwudiodoh64@gmail.com>
2023-10-30 13:34:35 +0100

    Scroll to top

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,14 @@ import { useSelector } from 'react-redux';
 import NewProduct from './pages/NewProduct';
 import ProductPage from './pages/ProductPage';
 import CategoryPage from './pages/CategoryPage';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   const user = useSelector((state) => state.user);
   return (
     <div className="App">
       <BrowserRouter>
+        <ScrollToTop />
         <Navigation />
         <Routes>
           <Route index element={<Home />} />
